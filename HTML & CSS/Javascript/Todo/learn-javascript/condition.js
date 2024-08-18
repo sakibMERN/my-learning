c7ff7915424aa7acd6c360c8a15add2c65c64a8e
sakibMERN <sakib.sr57@gmail.com>
2024-08-18 15:10:37 +0600

    first commit

---
+++ HTML & CSS/Javascript/Todo/learn-javascript/condition.js
@@ -0,0 +1,77 @@
+// if(3<5){
+//     console.log("I am true");
+// }
+
+// var myName = "Sakib";
+// if(myName==='Sakib'){
+//     console.log("You are allowed.");
+// }
+
+// var myAge = 21;
+// if(9<10){
+//     console.log('good to go');
+// }
+// if(myAge>= 20){
+//     console.log('You are adult');
+// }
+// if(myAge<20){
+//     console.log('You are not adult');
+// }
+
+// var myAge = 21;
+// if(myAge > 20 && myAge < 60) {
+//     console.log('You are a young person');
+// }
+
+// if...else statement
+
+// var age = 15;
+// if(age > 18) {
+//     console.log('You are adult');
+// } else{
+//     console.log("You are not adult");
+// }
+
+//if...else if statement
+
+// var whatDay= 'Thursday';
+
+// if(whatDay==='Friday'){
+//     console.log('It is close today');
+// }else if(whatDay==='Thursday'){
+//     console.log('It is partially closed');
+// }else if(whatDay==='Saturday'){
+//     console.log('Some office are close today');
+// }else{
+//     console.log('It normal working day today');
+// }
+
+//Nesting condition
+
+// var num = 1;
+// if(num > 1) {
+//     if(num >10) {
+//         console.log('Greater than 10');
+//     } else {
+//         console.log('Some where between 2 - 10');
+//     }
+// }
+
+//Find the biggest number of three number
+
+var a = 38;
+var b = 30;
+var c = 40;
+
+if (a > b) {
+  console.log("The biggest number is 38");
+} else if(a<b){
+  console.log("The biggest number is 30");
+} else if(a<c){
+    console.log('The biggest number is a');
+}else if (b>c){
+    console.log('The biggest number is b');
+}
+else{
+    console.log('The biggest number is c');
+}
