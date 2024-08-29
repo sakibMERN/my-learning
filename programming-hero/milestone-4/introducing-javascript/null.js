cd9f20d5c9d1ffe65269ae54fa1c2056d5805e67
sakibMERN <sakib.sr57@gmail.com>
2024-08-29 20:11:20 +0600

    practice javascript arithmatic operator, number, boolean, string,concatination, primative data types

---
+++ programming-hero/milestone-4/introducing-javascript/null.js
@@ -0,0 +1,14 @@
+var a ;
+
+console.log(a);
+
+
+
+// console.log(typeof value);
+
+// var value = '10';
+var value = "kabir";
+
+console.log(value);
+console.log(typeof value);
+console.log(isNaN(value));
---
+++ programming-hero/milestone-4/introducing-javascript/type.js
@@ -0,0 +1,17 @@
+// String 
+var name = "Sakib";
+var country = "Bangladesh";
+
+console.log(country);
+console.log(typeof country);
+
+//Boolean
+var isRich = true;
+console.log(isRich);
+console.log(typeof isRich);
+console.log(name);
+
+//Number 
+var price = 550;
+console.log(price);
+console.log(typeof price);
