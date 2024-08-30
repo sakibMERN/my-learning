6be354880ef6e7addb6684a2e3749e130c5d13a5
sakibMERN <sakib.sr57@gmail.com>
2024-08-31 00:25:31 +0600

    practicing if else task

---
+++ programming-hero/milestone-4/conceptual-1/basic.js
@@ -0,0 +1,25 @@
+// var me = 53;
+// me = 40;
+// console.log(me);
+// me = 20;
+// console.log(me);
+
+//data type => number , string, boolean, undefined, NaN, null.
+
+var x = 5;
+console.log(typeof x);
+
+var y = '5';
+console.log(typeof y);
+
+var z = true;
+console.log(typeof z);
+
+var notDeclear;
+console.log(typeof notDeclear);
+
+notDeclear = '3';
+console.log(typeof notDeclear);
+
+var nan = NaN;
+console.log(typeof nan);
---
+++ programming-hero/milestone-4/module-2/task-1.js
@@ -0,0 +1,15 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let BurgerPrice = 400;
+
+if (BurgerPrice > 500) {
+    console.log("You can take a coke for free.");
+}
+else {
+    console.log("You you have to pay for coke.");
+}
